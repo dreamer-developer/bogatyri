4abbf7cce2e2a86d35f7ca55fa280bbc41d0f127
Polina <dasyuk.polina@gmail.com>
2024-02-19 16:30:47 +0300

    Заменен фон

--- src/Enemies/Enemy.js
+++ src/Enemies/Enemy.js
@@ -21,14 +21,14 @@ class Enemy {
     }
 
     draw(context) {
-        //if (this.game.debug) { // в режиме дебага
+        if (this.game.debug) { // в режиме дебага
             // нарисуем персонажу (врагу) рамку
             context.strokeRect(this.x, this.y, this.width, this.height);
             // и отобразим у каждого врага его жизни
             context.fillStyle = 'yellow'; // цвет количества жизней
             context.font = '20px Helvetica';
             context.fillText(this.lives, this.x, this.y - 5);
-        //}
+        }
         // Метод drawImage элемента canvas с 9-ю аргументами для "вырезания" кадров из спрайтшита
         context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height,
             this.x, this.y, this.width, this.height);
